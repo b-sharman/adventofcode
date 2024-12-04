35441b16c2b7dbd1233fff366da21b406ca20a6a
b-sharman <byron.n.sharman@gmail.com>
2024-12-03 22:57:51 -0700

    2024: day 4 - inline string generation for part 2
    same idea as the previous commit

--- 2024/04/part2.py
+++ 2024/04/part2.py
@@ -7,9 +7,6 @@ s=0
 
 for y in range(len(grid)-2):
     for x in range(len(grid[0])-2):
-        sr=grid[y][x:x+3]
-        for i in (1,2):
-            sr+=grid[y+i][x:x+3]
-        s+=pat.fullmatch(sr)is not None
+        s+=pat.fullmatch("".join(grid[y+i][x:x+3]for i in range(3)))is not None
 
 print(s)
