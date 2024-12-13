54a85e5d1823ad97d7f58eb6accefe402ec5fee1
b-sharman <byron.n.sharman@gmail.com>
2024-12-12 22:24:06 -0700

    2024: day 13
    nothing like a bit of good ol' algebra

---
+++ 2024/13/part1.py
@@ -0,0 +1,22 @@
+with open("13") as f:
+    pzs=f.read().strip().split("\n\n")
+sm=0
+for pz in pzs:
+    lines = pz.split("\n")
+    xs,ys=lines[0].split(": ")[1].split(", ")
+    ax=int(xs[2:])
+    ay=int(ys[2:])
+    xs,ys=lines[1].split(": ")[1].split(", ")
+    bx=int(xs[2:])
+    by=int(ys[2:])
+    xprz,yprz=lines[2].split(": ")[1].split(", ")
+    x=int(xprz[2:])
+    y=int(yprz[2:])
+
+    a=(bx*y-by*x)/(bx*ay-by*ax)
+    b=(ax*y-ay*x)/(ax*by-ay*bx)
+
+    if a>=0 and b>=0 and a==int(a) and b==int(b):
+        sm+=3*a+1*b
+
+print(int(sm))
